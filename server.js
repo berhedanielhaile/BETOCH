454c9737652a3e76583db054166e56fb5f3ffda2
daniel haile <dannyyo1985.1931@gmail.com>
2026-08-22 02:09:39 +0300

    fix: make Express app Vercel serverless-compatible

--- server.js
+++ server.js
@@ -21,37 +21,27 @@ mongoose
   .connect(db)
   .then(async () => {
     console.log('DB connection successful!');
-
-    // try {
-    //   await mongoose.connection.collection('users').dropIndex('phone_1');
-    //   console.log('Removed legacy phone index from users collection');
-    // } catch (err) {
-    //   if (err.codeName !== 'IndexNotFound' && err.code !== 27) {
-    //     console.log('Index cleanup warning:', err.message);
-    //   }
-    // }
   })
   .catch((err) => console.log('DB connection error:', err));
 
-const PORT = process.env.PORT || 8000;
-const server = app.listen(PORT, () => console.log(`listening from port ${PORT}...`));
+if (require.main === module && !process.env.VERCEL) {
+  const PORT = process.env.PORT || 8000;
+  const server = app.listen(PORT, () => console.log(`listening from port ${PORT}...`));
 
-process.on('unhandledRejection', (err) => {
-  console.log('UNHANDELD REDJECTION: 💥 shutting down');
-  console.log(err.name, err.message);
-  server.close(() => {
-    process.exit(1);
+  process.on('unhandledRejection', (err) => {
+    console.log('UNHANDELD REDJECTION: 💥 shutting down');
+    console.log(err.name, err.message);
+    server.close(() => {
+      process.exit(1);
+    });
   });
-});
-process.on('SIGTERM', () => {
-  console.log('SIGTERM received, shutting down gracefully');
-  server.close(() => {
-    console.log('HTTP server closed');
-    process.exit(0);
+  process.on('SIGTERM', () => {
+    console.log('SIGTERM received, shutting down gracefully');
+    server.close(() => {
+      console.log('HTTP server closed');
+      process.exit(0);
+    });
   });
-});
+}
 
-// process.on("SIGINT", () => {
-//   console.log("SIGINT received, shutting down");
-//   server.close(() => process.exit(0));
-// });
+module.exports = app;
